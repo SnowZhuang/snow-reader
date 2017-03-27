50f53c0742b13e823148ae19ccbf4607ba4e7389
marcos <wengmeishi@hotmail.com>
2017-03-27 21:10:40 +0800

    订阅项功能新增

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $.ajax({
     "method":"get"
 }).done(function(categorys){
 	$.each(categorys,function(index,category){
-		$("#category-left-bar")
+		$("#categoryList")
 		.append('<li role="presentation"><a href="#">'+category.categoryName+'</a></li>')
 	})
 });
@@ -13,40 +13,20 @@ $.ajax({
     "method":"get"
 }).done(function(subscribeItems){
 	$.each(subscribeItems,function(index,subscribeItem){
-		var panel = $('<div class="pannel" style="overflow:hidden;">');
-		panel.append('<div class="book-name pull-left " style="padding:10px">'+subscribeItem.subscribeName+'</div>')
-		     .append('<div  class="pull-right"> <button class="btn btn-default  " >订阅</button> </div>')
-			 .append('<div class="book-instruction" >'+subscribeItem.subscribeInstruction+' </div>')
-			 .append('<div class="book-footer pull-right ">g12:45 ? 27082 人订阅 ? RSS</div>')
-		$("#subcribe-contain").append(panel);
+		// var panel = $('<div class="pannel" ');
+		// panel.append('<div class="book-name pull-left " style="padding:10px">'+subscribeItem.subscribeName+'</div>')
+		//      .append('<div  class="pull-right"> <button class="btn btn-default  " >订阅</button> </div>')
+		// 	 .append('<div class="book-instruction" >'+subscribeItem.subscribeInstruction+' </div>')
+		// 	 .append('<div class="book-footer pull-right ">g12:45 ? 27082 人订阅 ? RSS</div>')
+		// $("#subcribe-contain").append(panel);
+		var panel = $('<div class="bookContain">');
+		panel.append('<div class="book-name pull-left "><img src="images/1.jpg" width="20px" height="20px"><a >'+subscribeItem.subscribeName+'</a> </div>' )
+	         .append('<div  class="pull-right">  <button class="btn btn-default  " >订阅</button></div>') 
+	         .append('<div class="book-instruction " > '+subscribeItem.subscribeInstruction+'</div>')
+	         .append('<div class="book-subscribeMessage" style="text-align: right;">时间.订阅人数</div> ')
+	    $("#rbight-column").append(panel);
 	})
 });
 
 
-$("xxx").on("click",function(){
-	XXX
-})
-var TestObject = function(){
-	this.allEvents = {};
-}
 
-
-TestObject.prototype.on = function(eventName,callback) {
-	var events = allEvents[eventName];
-	if(events){
-		events = [];
-		allEvents[eventName] = events;
-	}
-	events.push(callback);
-};
-var testObject = new TestObject();
-testObject.on("click",function(){XXX})
-testObject.on("click",function(){XXX})
-
-
-click(event) = function(){
-	var callbacks = testObject.events['click'];
-	$.each(callbacks,function(index,callback){
-		callback(event);
-	});
-}
